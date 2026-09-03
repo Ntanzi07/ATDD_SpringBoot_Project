66bdb43a1cb9d252cb7ec6e1ed828913de96316b
DevOps <devops@labinfo.facens.br>
2026-09-02 22:23:17 -0300

    docs: first version of the ATDD.xlsx

--- src/main/java/org/grupo1/grupo_1_praticaatdd/UserApplication.java
+++ src/main/java/org/grupo1/grupo_1_praticaatdd/UserApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Grupo1PraticaAtddApplication {
+public class UserApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Grupo1PraticaAtddApplication.class, args);
+        SpringApplication.run(UserApplication.class, args);
     }
 
 }
